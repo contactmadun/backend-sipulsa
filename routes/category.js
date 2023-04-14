05e602cbfc4abe35a375d6a774566b5d3ce5f606
madun <contact.madun@gmail.com>
2023-04-14 21:11:22 +0700

    fixed api category

--- routes/category.js
+++ routes/category.js
@@ -4,11 +4,21 @@ const Validator = require('fastest-validator');
 const v = new Validator();
 const { Category } = require('../models');
 
+router.get('/', async (req,res) =>{
+    const categorys = await Category.findAll();
+    return res.json(categorys);
+});
+
+router.get('/:id', async (req,res) =>{
+    const id = req.params.id;
+    const category = await Category.findByPk(id);
+    return res.json(category || {});
+});
+
 router.post('/', async (req, res) => {
     const schema = {
         name: 'string'
     }
-
     const validate = v.validate(req.body, schema);
 
     if(validate.length){
@@ -20,6 +30,43 @@ router.post('/', async (req, res) => {
     const category = await Category.create(req.body);
     res.json(category); 
       
+});
+
+router.put('/:id', async (req, res) => {
+    const id = req.params.id;
+    let category = await Category.findByPk(id);
+
+    if(!category){
+        return res.json({message: 'Kategori tidak ditemukan'});
+    }
+
+    const schema = {
+        name: 'string|optional'
+    }
+    const validate = v.validate(req.body, schema);
+
+    if(validate.length){
+        return res
+        .status(400)
+        .json(validate);
+    }
+    
+    category = await category.update(req.body);
+    res.json(category); 
+});
+
+router.delete('/:id', async(req, res) => {
+    const id = req.params.id;
+    let category = await Category.findByPk(id);
+
+    if(!category){
+        return res.json({message: 'Kategori tidak ditemukan'});
+    }
+
+    await category.destroy();
+    res.json({
+        message: 'Kategori berhasil dihapus'
+    })
 });
 
 module.exports = router;
